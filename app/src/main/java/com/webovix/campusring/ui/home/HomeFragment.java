1522f5e62f6ff910dc325409ae5594f200a1d0f9
pranay <pranaybhatia80@gmail.com>
2020-08-07 17:47:43 +0530

    bug fix

--- app/src/main/java/com/webovix/campusring/ui/home/HomeFragment.java
+++ app/src/main/java/com/webovix/campusring/ui/home/HomeFragment.java
@@ -4,20 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-import android.widget.Toolbar;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
-import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.tabs.TabItem;
-import com.google.android.material.tabs.TabLayout;
 import com.webovix.campusring.R;
 
 public class HomeFragment extends Fragment {
@@ -28,15 +20,15 @@ public class HomeFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homefragment= inflater.inflate(R.layout.fragment_home , container, false);
-//        ViewPager viewPager = findViewById(R.id.viewPager);
-//        PageAdapter pageAdapter=new PageAdapter(getChildFragmentManager(),tabLayout.getTabCount());;
-//        TabLayout tabLayout = (homefragment)findViewById(R.id.tablayout);
-//        TabItem tabChats = (homefragment)findViewById(R.id.tabChats);
-//        TabItem tabNearby = findViewById(R.id.tabNearby);
-//        TabItem tabActive = findViewById(R.id.tabActive);
-//        TabItem tabGroups = findViewById(R.id.tabGroups);
-//
+        homefragment = inflater.inflate(R.layout.fragment_home, container, false);
+        ViewPager viewPager = homefragment.findViewById(R.id.viewPager);
+//       PageAdapter pageAdapter=new PageAdapter(getChildFragmentManager(),tabLayout.getTabCount());
+//        TabLayout tabLayout = homefragment.findViewById(R.id.tablayout);
+        TabItem tabChats = homefragment.findViewById(R.id.tabChats);
+        TabItem tabNearby = homefragment.findViewById(R.id.tabNearby);
+        TabItem tabActive = homefragment.findViewById(R.id.tabActive);
+        TabItem tabGroups = homefragment.findViewById(R.id.tabGroups);
+
         return homefragment;
     }
 }
